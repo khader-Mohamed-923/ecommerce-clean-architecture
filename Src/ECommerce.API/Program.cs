e3802a6375a955120e0f6dd3c5acf25a4a696da8
Khader Mohamed <khaderelhabal923@gmail.com>
2026-06-28 13:38:57 +0300

    chore: update packages, di registrations, and project configurations

--- Src/ECommerce.API/Program.cs
+++ Src/ECommerce.API/Program.cs
@@ -1,17 +1,33 @@
 using ECommerce.API;
 using ECommerce.Application;
 using ECommerce.Infrastructure;
+using ECommerce.Infrastructure.Persistence.Seeders;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApi();
 builder.Services.AddApplication();
-builder.Services.AddInfrastructure();
-
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<SeederCoordinator>();
+    await seeder.SeedAsync();
+}
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-
