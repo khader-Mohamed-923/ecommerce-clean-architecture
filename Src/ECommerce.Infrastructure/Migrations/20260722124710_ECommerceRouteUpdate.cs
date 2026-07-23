64ed8323a3ca10321903ff4b4123d7515017a1c4
Khader Mohamed <khaderelhabal923@gmail.com>
2026-07-23 13:25:17 +0300

    feat(infrastructure): add ECommerceRouteUpdate migration

---
+++ Src/ECommerce.Infrastructure/Migrations/20260722124710_ECommerceRouteUpdate.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ECommerceRouteUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductBrands_BrandId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Products",
+                newName: "PictureUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "BrandId",
+                table: "Products",
+                newName: "ProductBrandId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                newName: "IX_Products_ProductBrandId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTypes_Name",
+                table: "ProductTypes",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Name",
+                table: "Products",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Price",
+                table: "Products",
+                column: "Price");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBrands_Name",
+                table: "ProductBrands",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductBrands_ProductBrandId",
+                table: "Products",
+                column: "ProductBrandId",
+                principalTable: "ProductBrands",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductBrands_ProductBrandId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductTypes_Name",
+                table: "ProductTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Name",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Price",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductBrands_Name",
+                table: "ProductBrands");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductBrandId",
+                table: "Products",
+                newName: "BrandId");
+
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "ImageUrl");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_ProductBrandId",
+                table: "Products",
+                newName: "IX_Products_BrandId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductBrands_BrandId",
+                table: "Products",
+                column: "BrandId",
+                principalTable: "ProductBrands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
