3721c4157bb2dd776754b1e28fe3604eeb60e7e1
Khader Mohamed <khaderelhabal923@gmail.com>
2026-07-23 13:25:28 +0300

    chore(infrastructure): update DI registrations for new services

--- Src/ECommerce.Infrastructure/DependencyInjection.cs
+++ Src/ECommerce.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,9 @@
-﻿using ECommerce.Domain.Interfaces;
-using ECommerce.Infrastructure.Data.DbContexts;
+using ECommerce.Domain.Repositories;
+using ECommerce.Infrastructure.Persistence.DbContexts;
 using ECommerce.Infrastructure.Persistence.Interceptors;
 using ECommerce.Infrastructure.Persistence.Seeders;
-using ECommerce.Infrastructure.Persistence.UnitOfWork;
+using ECommerce.Infrastructure.Repositories;
+using ECommerce.Infrastructure.Caching;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,20 +12,33 @@ namespace ECommerce.Infrastructure;
 
 public static class DependencyInjection
 {
-
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton<AuditableEntityInterceptor>();
+        services.AddSingleton<IAuditInterceptor, AuditInterceptor>();
+        services.AddSingleton<ISoftDeleteInterceptor, SoftDeleteInterceptor>();
+
         services.AddDbContext<StoreDbContext>(options => 
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));  
         
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped(typeof(IReadRepository<>), typeof(Repository<>));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         services.AddScoped<ISeeder, BrandSeeder>();
         services.AddScoped<ISeeder, TypeSeeder>();
         services.AddScoped<ISeeder, ProductSeeder>();
         services.AddScoped<SeederCoordinator>();
+        
+        // Caching
+        services.AddSingleton<CacheEntryPolicyValidator>();
+#pragma warning disable EXTEXP0018 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        services.AddHybridCache();
+#pragma warning restore EXTEXP0018 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        services.AddScoped(typeof(ICachedAggregateStore<>), typeof(HybridCacheAggregateStore<>));
+        services.AddScoped<IBasketStore, HybridBasketStore>();
 
+        services.Configure<ECommerce.Application.Common.Settings.CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
+        services.AddScoped<ECommerce.Application.Common.Interfaces.IAttachmentService, ECommerce.Infrastructure.Services.AttachmentService>();
 
         return services;
     }
